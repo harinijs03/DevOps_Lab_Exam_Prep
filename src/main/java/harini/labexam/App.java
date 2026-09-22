cc62cdd3bb10fc7313f8a6342e80ee7c9624ebf0
harinijs03 <harinijs03@gmail.com>
2026-09-22 23:48:12 +0530

    First commit

---
+++ src/main/java/harini/labexam/App.java
@@ -0,0 +1,14 @@
+package harini.labexam;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public static String getMessage() {
+    	return "Hello World!";
+    }
+}
---
+++ src/test/java/harini/labexam/AppTest.java
@@ -0,0 +1,21 @@
+package harini.labexam;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void testHelloWorldMessage() {
+    	assertEquals("Hello World!", App.getMessage());
+    }
+    
+}
